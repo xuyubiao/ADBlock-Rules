d616b2e04dd28e279dc4ae0a97ef0d7ef06399a7
xuyubiao <biaodie@gmail.com>
2026-03-11 09:19:09 +0800

    deleted:    ADBlock.sh 	deleted:    ADBlock.sh.bak 	deleted:    AdBlock.txt 	deleted:    Ban.sh 	deleted:    Shadowrocket-ADBlock-Rules.cfg 	modified:   fakenews-gambling-porn.list 	new file:   fakenews-gambling-porn.sh 	deleted:    gfwlist.txt 	new file:   hosts 	new file:   hosts.1 	new file:   hosts.2

--- Ban.sh
+++
@@ -1,2 +0,0 @@
-wget https://raw.githubusercontent.com/StevenBlack/hosts/refs/heads/master/alternates/fakenews-gambling-porn-only/hosts
-grep -v '#' hosts |awk  '{if(NF==2) print $2}' |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |sort -du  > fakenews-gambling-porn.list
---
+++ fakenews-gambling-porn.sh
@@ -0,0 +1,5 @@
+
+URL=http://sbc.io/hosts/alternates/fakenews-gambling-porn/hosts
+wget $URL -O hosts
+
+grep -v '#' hosts |awk  '{if(NF==2) print $2}' |grep "[a-z]" |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |sort -du  > fakenews-gambling-porn.list
