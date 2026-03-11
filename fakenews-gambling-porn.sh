535704ac1ae7f0c07d564b1db52a51c5aea74966
xuyubiao <biaodie@gmail.com>
2026-03-11 09:20:22 +0800

    modified:   fakenews-gambling-porn.sh 	deleted:    hosts 	deleted:    hosts.1 	deleted:    hosts.2

--- fakenews-gambling-porn.sh
+++ fakenews-gambling-porn.sh
@@ -3,3 +3,5 @@ URL=http://sbc.io/hosts/alternates/fakenews-gambling-porn/hosts
 wget $URL -O hosts
 
 grep -v '#' hosts |awk  '{if(NF==2) print $2}' |grep "[a-z]" |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |sort -du  > fakenews-gambling-porn.list
+
+rm -f hosts 2>/dev/null
