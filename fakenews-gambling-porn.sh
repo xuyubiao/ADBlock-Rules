8c7700932a398f1904955522ed5fdbbea8c6a13c
xuyubiao <biaodie@gmail.com>
2026-03-17 08:21:22 +0800

    更新广告过滤规则

--- fakenews-gambling-porn.sh
+++ fakenews-gambling-porn.sh
@@ -2,6 +2,15 @@
 URL=http://sbc.io/hosts/alternates/fakenews-gambling-porn/hosts
 wget $URL -O hosts
 
-grep -v '#' hosts |awk  '{if(NF==2) print $2}' |grep "[a-z]" |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |sort -du  > fakenews-gambling-porn.list
+grep -v '#' hosts |awk  '{if(NF==2) print $2}' |grep "[a-z]" |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |grep "[a-z]" |sort -du  > fakenews-gambling-porn.list
 
 rm -f hosts 2>/dev/null
+
+######################################################
+GFW=https://raw.githubusercontent.com/gfwlist/gfwlist/master/list.txt
+wget $GFW -O list.txt
+
+egrep -v '@|!|http:|/' list.txt | sed -E 's#^\|{1,2}(https://)?##' |grep "[a-z]" |awk -F"." '{if(NF>=2) print "DOMAIN-SUFFIX,"$(NF-1)"."$NF}' |grep "[a-z]" |sort -du > gwlist.txt
+
+rm -f list.txt 2>/dev/null
+
